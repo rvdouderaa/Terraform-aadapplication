933f2221d0e1a25f93388aa8303c1a430387aeeb
Roel Ouderaa, van der <code@buildthecloud.io>
2020-04-17 15:10:05 +0200

    added variables

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
-# vscode test
 provider "azurerm" {
   version = "=1.42.0"
   subscription_id = var.subscription_id
 }
 
-
-resource "azuread_application" "example" {
-  name                       = "myTestapplication"
-  identifier_uris            = ["https://www.simac.com/myTestApplication"]
-  reply_urls                 = ["https://localhost"]
+resource "azuread_application" "AADApplication" {
+  name                       = "${var.workloadName}-${var.environment}-${var.postfix}"
+  identifier_uris            = ["https://www.simac.com/${var.workloadName}-${var.environment}-${var.postfix}"]
+  reply_urls                 = [var.replyUrl]
   available_to_other_tenants = false
   oauth2_allow_implicit_flow = false    
 }
@@ -20,13 +18,12 @@ resource "random_password" "default" {
 }
 
 resource "azuread_application_password" "default" {
-  application_object_id = azuread_application.example.id
+  application_object_id = azuread_application.AADApplication.id
   value                 = random_password.default.result
   end_date              = "2040-01-01T01:02:03Z"
 }
 
 resource "azuread_service_principal" "default" {
-  application_id = azuread_application.example.application_id
+  application_id = azuread_application.AADApplication.application_id
   tags  = []
 }
-
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable workloadName {
+    type=string
+    default = "myApplication"
+}
+variable environment {
+    type=string
+    default = "development"
+}
+
+variable replyUrl {
+    type=string
+    default = "https://localhost"
+}
+
+variable postfix {
+    type=string
+    default = "appreg"
+}
