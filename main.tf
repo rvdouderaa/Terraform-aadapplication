2b1ec45eafe5cd87fe3830816979f8d5acbe4eb6
Roel Ouderaa, van der <code@buildthecloud.io>
2020-04-20 11:41:05 +0200

    Moved end_date to locals

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+locals {
+
+  end_date = timeadd(timestamp(), "9000h")
+
+}
+
 resource "azuread_application" "AADApplication" {
   name                       = "${var.workloadName}-${var.environment}-${var.postfix}"
   identifier_uris            = ["https://www.simac.com/${var.workloadName}-${var.environment}-${var.postfix}"]
@@ -19,7 +25,7 @@ resource "random_password" "default" {
 resource "azuread_application_password" "default" {
   application_object_id = azuread_application.AADApplication.id
   value                 = random_password.default.result
-  end_date              = timeadd(timestamp(),"9000h")
+  end_date              = local.end_date
 }
 
 resource "azuread_service_principal" "default" {
@@ -38,3 +44,9 @@ output "Secret" {
 output "ValidThru" {
   value = azuread_application_password.default.end_date
 }
+
+# Outputs:
+# 
+# Secret = u0r3oz9Zk<g3>2Z&dnItZy=t<kefztPxy
+# ValidThru = 2021-04-30T09:35:12Z
+# applicationID = c168a97a-2517-4841-8f50-1d5c94b6cbdf
